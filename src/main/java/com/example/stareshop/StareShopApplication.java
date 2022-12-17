fe1c6d21d20abf53a3fc58116c4e5275cd911d0f
Sorin37 <glizard.glizard@yahoo.com>
2022-12-17 10:31:55 +0200

    First commit

---
+++ src/main/java/com/example/stareshop/StareShopApplication.java
@@ -0,0 +1,11 @@
+package com.example.stareshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StareShopApplication {
+
+	public static void main(String[] args) { SpringApplication.run(StareShopApplication.class, args); }
+
+}
---
+++ src/test/java/com/example/stareshop/StareShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stareshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StareShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
