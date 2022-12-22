6731f98d1bf67a83c0716bceafce23b8b4ee0800
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2022-12-22 14:51:33 +0200

    Created WebSecurityConfig file and made small changes to application.properties file.

---
+++ src/main/java/com/example/stareshop/config/WebSecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.stareshop.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig implements WebMvcConfigurer {
+    private static final String[] METHODS_ALLOWED = {
+            HttpMethod.GET.name(),
+            HttpMethod.POST.name(),
+            HttpMethod.PUT.name(),
+            HttpMethod.PATCH.name(),
+            HttpMethod.DELETE.name(),
+    };
+
+    @Bean
+    SecurityFilterChain resources (HttpSecurity http) throws Exception{
+       http.authorizeHttpRequests()
+               .requestMatchers(HttpMethod.GET).hasAnyAuthority("Aaa", "cxcc")
+               .requestMatchers(METHODS_ALLOWED).hasAnyAuthority("sddsa", "Asdasd", "asdasd", "asdasda", "asdasda", "asdasdad", "SDFSFGSFGdfg")
+               .anyRequest().authenticated()
+               .and()
+               .formLogin()
+               .loginPage("/login").permitAll()
+               .and()
+               .logout()
+               .logoutSuccessUrl("/login").permitAll();
+       return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(METHODS_ALLOWED)
+                .allowedOrigins("*");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
+
+/*http
+                .csrf().disable()
+                .authorizeHttpRequests((requests) -> {
+                    try {
+                        requests
+                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                                .requestMatchers("/**").permitAll()
+                                .and()
+                                .formLogin()
+                                .loginPage("/login")
+                                .permitAll()
+                                .and()
+                                .logout()
+                                .logoutSuccessUrl("/login")
+                                .permitAll();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .httpBasic();
+
+        return http.build();*/
